198f8faae86f1b1d7d20a192ecea2aae17dc03cf
melkir <melkir13@gmail.com>
2013-05-22 17:44:15 +0200

    add some interface

--- src/Sdz/SiteBundle/Controller/DefaultController.php
+++ src/Sdz/SiteBundle/Controller/DefaultController.php
@@ -10,4 +10,19 @@ class DefaultController extends Controller
     {
         return $this->render('SdzSiteBundle:Default:index.html.twig');
     }
+
+    public function contactAction()
+    {
+        return $this->render('SdzSiteBundle:Default:contact.html.twig');
+    }
+
+    public function lienAction()
+    {
+        return $this->render('SdzSiteBundle:Default:lien.html.twig');
+    }
+
+    public function roadmapAction()
+    {
+        return $this->render('SdzSiteBundle:Default:roadmap.html.twig');
+    }
 }
