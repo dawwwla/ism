d5587905fab67c10bff1ce967161a50dc64b3be1
melkir <melkir13@gmail.com>
2013-06-02 22:24:22 +0200

    regroup method

--- src/Ism/BlogBundle/Controller/ArticleController.php
+++ src/Ism/BlogBundle/Controller/ArticleController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 
 use Ism\BlogBundle\Entity\Article;
+use Ism\BlogBundle\Entity\Membre;
 use Ism\BlogBundle\Entity\Commentaire;
 
 use Ism\BlogBundle\Form\ArticleType;
@@ -24,49 +25,6 @@ use Ism\BlogBundle\Bigbrother\MessagePostEvent;
 class ArticleController extends Controller
 {
     /**
-     * Creates a new Article entity.
-     * @Secure(roles="ROLE_AUTEUR")
-     */
-    public function createAction(Request $request)
-    {
-        $article  = new Article();
-
-        if ($user = $this->getUser()) {
-            // On définit le User par défaut dans le formulaire (utilisateur courant)
-            $article->setUser($user);
-        }
-
-        $form = $this->createForm(new ArticleType(), $article);
-        $form->bind($request);
-
-        if ($form->isValid()) {
-            // --- Début de notre fonctionnalité BigBrother ---
-            // On crée l'évènement
-            $event = new MessagePostEvent($article->getContenu(), $this->getUser());
-
-            // On déclenche l'évènement
-            $this->get('event_dispatcher')
-                 ->dispatch(BigbrotherEvents::onMessagePost, $event);
-
-             // On récupère ce qui a été modifié par le ou les listener(s), ici le message
-             $article->setContenu($event->getMessage());
-            // --- Fin de notre fonctionnalité BigBrother ---
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($article);
-            $em->flush();
-
-            $this->get('session')->getFlashBag()->add('info', 'Article bien ajouté');
-            return $this->redirect($this->generateUrl('article_show', array('slug' => $article->getSlug())));
-        }
-
-        return $this->render('IsmBlogBundle:Article:new.html.twig', array(
-            'article' => $article,
-            'form'   => $form->createView(),
-        ));
-    }
-
-    /**
      * Displays a form to create a new Article entity.
      * @Secure(roles="ROLE_AUTEUR")
      */
@@ -74,7 +32,40 @@ class ArticleController extends Controller
     {
         $article = new Article();
 
-        $form   = $this->createForm(new ArticleType(), $article);
+        $form = $this->createForm(new ArticleType(), $article);
+
+        $request = $this->getRequest();
+
+        if ($request->getMethod() == 'POST') {
+
+            if ($user = $this->getUser()) {
+                // On définit le User par défaut dans le formulaire (utilisateur courant)
+                $article->setUser($user);
+            }
+
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                // --- Début de notre fonctionnalité BigBrother ---
+                // On crée l'évènement
+                $event = new MessagePostEvent($article->getContenu(), $this->getUser());
+
+                // On déclenche l'évènement
+                $this->get('event_dispatcher')
+                     ->dispatch(BigbrotherEvents::onMessagePost, $event);
+
+                 // On récupère ce qui a été modifié par le ou les listener(s), ici le message
+                 $article->setContenu($event->getMessage());
+                // --- Fin de notre fonctionnalité BigBrother ---
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($article);
+                $em->flush();
+
+                $this->get('session')->getFlashBag()->add('info', 'Article bien ajouté');
+                return $this->redirect($this->generateUrl('article_show', array('slug' => $article->getSlug())));
+            }
+        }
 
         return $this->render('IsmBlogBundle:Article:new.html.twig', array(
             'article' => $article,
